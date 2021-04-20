81aad5420994df1dc94c48534b7a3061eb85e08d
Gurjit Sangha <gsangha494@gmail.com>
2021-04-21 00:07:50 +0100

    redis disconnect

--- pages/api/guess.js
+++ pages/api/guess.js
@@ -1,9 +1,10 @@
-import redis from "../../lib/redis";
+import redis, { disconnect } from "../../lib/redis";
 
 export default async (req, res) => {
   const { id, guess } = req.body;
 
   const subreddit = await redis.get(id);
+  disconnect();
 
   if (!subreddit) {
     res.status(404).send("Unable to find game id");
--- pages/api/random.js
+++ pages/api/random.js
@@ -1,11 +1,11 @@
 import { getPosts } from "../../lib/reddit";
-import redis from "../../lib/redis";
+import redis, { disconnect } from "../../lib/redis";
 import { v4 as uuidv4 } from "uuid";
 
 export default async (req, res) => {
   const id = uuidv4();
 
-  const posts = await getPosts({ subreddit: "random" });
+  const posts = await getPosts({ subreddit: "random", limit: 25 });
 
   if (!posts) {
     res.status(500).send("Unable to load random posts");
@@ -15,6 +15,7 @@ export default async (req, res) => {
   // get the subreddit name and save
   const subreddit = posts[0].subreddit;
   redis.set(id, subreddit);
+  disconnect();
 
   res.status(200).json({ id, posts });
 };
